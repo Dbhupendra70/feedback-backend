8c29af6002c54673f842b6753da58da4287f65ab
Dbhupendra70 <bhupendra4.code@gmail.com>
2025-05-20 18:02:55 +0530

    updated again to fix it database_url in db.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'feedback.db')}?check_same_thread=False"
+DATABASE_URL = "sqlite:///./app/feedback.db"
 
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
